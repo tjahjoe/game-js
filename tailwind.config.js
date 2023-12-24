7d08a16bea3d70eaf139849e825767b213d3a0db
GE08T <gaerthamgk@gmail.com>
2023-12-24 21:50:46 +0700

    struggle to find design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "*",
-  ],
+  content: ["*.{html,js,css}"],
   theme: {
+    colors : {
+      'dark' : '#111827',
+      'light' : '#e0e7ff',
+      'slate' : '#78716c',
+      'acsent' : '#0ea5e9',
+    },
     extend: {},
   },
   plugins: [],
