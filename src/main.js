04a82d437d3c80790c0fc600043e66df04f5442b
GE08T <gaerthamgk@gmail.com>
2024-01-14 18:16:03 +0700

    feat: adding mockup for mobile layout

--- src/main.js
+++ src/main.js
@@ -1,2 +1,6 @@
 import '../styles/main.css';
+import '../styles/utils.css';
 import '../styles/modern-normalize.css';
+import '../styles/components/header.css';
+import '../styles/components/mobile-filter.css';
+import '../styles/components/game.css';
