3eb9897daedea456187d882b5140817754b1c43f
GE08T <gaerthamgk@gmail.com>
2024-01-26 23:09:40 +0700

    chore(styles): add dark scheme with user default theme

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
+import '../styles/modern-normalize.css';
+
 import '../styles/main.css';
 import '../styles/utils.css';
-import '../styles/modern-normalize.css';
+
+// Components
 import '../styles/components/header.css';
 import '../styles/components/mobile-filter.css';
 import '../styles/components/game.css';
